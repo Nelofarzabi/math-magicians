96b158046acd334f0aca0625909d8a3c208a5105
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-08-12 00:50:56 -0700

    fetch data

---
+++ src/components/quotes.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles/quotes.css';
+
+export default function Quotes(props) {
+  const { text, author } = props;
+
+  return (
+    <>
+      <section className="quotes">
+        <q>{text}</q>
+        <p className="author">
+          <i>
+            -
+            {author}
+          </i>
+        </p>
+      </section>
+    </>
+  );
+}
+
+Quotes.propTypes = {
+  text: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
