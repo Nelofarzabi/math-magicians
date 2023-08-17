d9572df82a833634b36b2609bceca15567038aae
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-08-17 01:22:09 -0700

    Add full website

--- src/components/fetchData.jsx
+++ src/components/fetchData.jsx
@@ -28,6 +28,10 @@ export default function FetchData() {
     return <div className="error">Something went wrong!</div>;
   }
   return (
-    isLoading ? <div className="space">Loading... </div> : <Quotes text={quote.quote} author={quote.author} />
+    <div className="container div-center">
+      {
+        isLoading ? <div className="space">Loading... </div> : <Quotes text={quote.quote} author={quote.author} />
+      }
+    </div>
   );
 }
